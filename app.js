7f7ef5a654e5db7fa5997c5d7851e1cbb208d31c
Unknown <kasper50@live.dk>
2017-12-08 17:03:35 +0100

    working now!

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const port = process.env.PORT || 3000;
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('Example app listening on port 3000!'))
